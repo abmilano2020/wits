a1bfe6bff3c114ae81da01f4f8dfdf70eb42a32c
abe <andrea_belcastro@yahoo.it>
2025-09-02 22:44:36 +0200

    feat: added teams weights (T4)

--- main.py
+++ main.py
@@ -7,50 +7,45 @@ import os
 df = pd.read_csv("par_team_city.csv")
 
 # to be fetched automatically
-city_weight = {
-    "Bergamo":8.82,
-    "Bologna":2.34,
-    "Cagliari":0.01,
-    "Como":0.77,
-    "Cremona":0.05,
-    "Firenze":2.47,
-    "Genova":0.11,
-    "Verona":0.01,
-    "Milano":44.05,
-    "Lecce":0.02,
-    "Napoli":16.61,
-    "Parma":0.06,
-    "Pisa":0.01,
-    "Roma":0,
-    "Sassuolo":0,
-    "Torino":9.26,
-    "Udine":0.07
+team_weight = {
+    "Atalanta": 7.47,
+    "Bologna": 3.47,
+    "Cagliari": 0.02,
+    "Como": 0.60,
+    "Cremonese": 0.11,
+    "Fiorentina": 2.25,
+    "Genoa": 0.12,
+    "Hellas Verona": 0,
+    "Inter": 25.14,
+    "Milan": 5.13,
+    "Juventus": 13.01,
+    "Lazio": 3.34,
+    "Lecce": 0,
+    "Napoli": 20.86,
+    "Parma": 0.07,
+    "Pisa": 0,
+    "Roma": 17.97,
+    "Sassuolo": 0,
+    "Torino": 0.25,
+    "Udinese": 0.19
 }
 
-# group lat & lon by city, for cities with > 1 team
-df_grouped = df.groupby(df.columns[1]).agg({
-    df.columns[2]: 'mean',
-    df.columns[3]: 'mean'
-}).reset_index()
-
-df_grouped.columns = ['city', 'lat', 'lon']
-
-# add weights
-df_grouped['weight'] = df_grouped['city'].map(city_weight)
+# add team's weights (title chances)
+df['weight'] = df['Squadra'].map(team_weight)
 
 # get lat/lon/weight
-lat = df_grouped['lat'].values
-lon = df_grouped['lon'].values
-weight = df_grouped['weight'].values
+lat = df['Latitudine'].values
+lon = df['Longitudine'].values
+weight = df['weight'].values
 
-# calculate center
+# calculate weighted center
 avg_lat = np.average(lat, weights=weight)
 avg_lon = np.average(lon, weights=weight)
 
 # get Roma's coordinates to centre map
-roma_row = df[df.iloc[:,1] == "Roma"].iloc[0]
-lat_roma = float(roma_row.iloc[2])
-lon_roma = float(roma_row.iloc[3])
+roma_row = df[df['Città'] == "Roma"].iloc[0]
+lat_roma = float(roma_row['Latitudine'])
+lon_roma = float(roma_row['Longitudine'])
 
 # create map
 m = folium.Map(location=[lat_roma, lon_roma], zoom_start=6, tiles='Esri.WorldImagery')
@@ -71,9 +66,9 @@ folium.Marker(
 football_design = {"prefix": "fa", "color": "blue", "icon": "futbol"}
 football_icon = folium.Icon(**football_design)
 
-for _, row in df_grouped.iterrows():
+for _, row in df.iterrows():
     folium.Marker(
-        location=[row['lat'], row['lon']],
+        location=[row['Latitudine'], row['Longitudine']],
         icon=football_icon
     ).add_to(m)
 
